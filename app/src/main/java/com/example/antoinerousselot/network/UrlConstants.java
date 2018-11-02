c32f3766817e3a94f0f056d4b0e552560712436e
Francois Carlue <francois.carlue@centrale-marseille.fr>
2018-11-02 12:37:48 +0100

    Added acceleration detection (in any direction) + Changed logo of application + updated URLs to oignon's OVH

--- app/src/main/java/com/example/antoinerousselot/network/UrlConstants.java
+++ app/src/main/java/com/example/antoinerousselot/network/UrlConstants.java
@@ -1,9 +1,9 @@
 package com.example.antoinerousselot.network;
 
 public interface UrlConstants {
-    String GET_URL = "http://node.melisse.ovh1.ec-m.fr/";
-    String POST_URL = "http://node.melisse.ovh1.ec-m.fr/handlePost";
-    String POST_URL_AUTH_PLAYER1 = "http://node.melisse.ovh1.ec-m.fr/authenticationPlayer1";
+    String GET_URL = "http://node.oignon.ovh1.ec-m.fr/";
+    String POST_URL = "http://node.oignon.ovh1.ec-m.fr/handlePost";
+    String POST_URL_AUTH_PLAYER1 = "http://node.oignon.ovh1.ec-m.fr/authenticationPlayer1";
 
     int GET_URL_REQUEST_CODE = 1;
     int POST_URL_REQUEST_CODE = 2;
--- app/src/main/java/com/example/antoinerousselot/testvolley/SecondActivity.java
+++ app/src/main/java/com/example/antoinerousselot/testvolley/SecondActivity.java
@@ -1,20 +1,30 @@
 package com.example.antoinerousselot.testvolley;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.widget.Toast;
 
 import com.example.antoinerousselot.gesture.DetectSwipeGestureListener;
 
-public class SecondActivity extends AppCompatActivity implements GestureDetector.OnDoubleTapListener{
+public class SecondActivity extends AppCompatActivity implements GestureDetector.OnDoubleTapListener, SensorEventListener {
 
     private static final String DEBUG_TAG = "Gestures";
 
     // This is the gesture detector compat instance.
     private GestureDetectorCompat gestureDetectorCompat = null;
+
+    private long lastUpdate;
+    private SensorManager sensorManager;
+    private Sensor mSensor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +40,41 @@ public class SecondActivity extends AppCompatActivity implements GestureDetector
         // Create the gesture detector with the gesture listener.
         gestureDetectorCompat = new GestureDetectorCompat(this, gestureListener);
 
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        lastUpdate = System.currentTimeMillis();
+
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            getAccelerometer(event);
+        }
+
+    }
+
+    private void getAccelerometer(SensorEvent event) {
+        float[] values = event.values;
+        // Movement
+        float x = values[0];
+        float y = values[1];
+        float z = values[2];
+
+        float accelationSquareRoot = (x * x + y * y + z * z)
+                / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
+        long actualTime = event.timestamp;
+        if (accelationSquareRoot >= 6) //
+        {
+            lastUpdate = actualTime;
+            Toast.makeText(this, "Acceleration detected", Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
 
     @Override
@@ -56,5 +101,22 @@ public class SecondActivity extends AppCompatActivity implements GestureDetector
     public boolean onSingleTapConfirmed(MotionEvent event) {
         Log.d(DEBUG_TAG, "onSingleTapConfirmed: " + event.toString());
         return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // register this class as a listener for the orientation and
+        // accelerometer sensors
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        // unregister listener
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 }
